46c5cfd9adee64cfc6aaea554feae2a89c7d02fc
ansonfoong <afoong@albany.edu>
2019-01-20 08:13:56 -0500

    Total all of the costs and multiply by the tax rate

--- giftbox.py
+++ giftbox.py
@@ -8,5 +8,13 @@ milkChocolateOrangeCream = 1
 milkChocolateCaramel = 1.05
 whiteChocolateCoffeeCreams = 1.25
 whiteChocolatePecans = 1.30
+SALES_TAX = .0825
 
-print("Welcome to the Candy Shop. You can choose any three varieties of chocolate, creating a box of six chocolates.")
+print("Welcome to the Candy Shop. We will pick a combination of three chocolates, creating a box of six chocolates.")
+print("-----------------------------------------\n")
+print("We have selected:\nDark Chocolate Hazelnut, White Chocolate Coffee Creams, and Milk Chocolate Caramel.")
+
+totalPrice = (darkChocolateHazelNut * 2) + (whiteChocolateCoffeeCreams * 2) + (milkChocolateCaramel * 2)
+tax = totalPrice * SALES_TAX
+totalPrice += tax
+print(totalPrice)
