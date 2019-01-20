765823c73a75a30d65a98adaf7c72d463c4b51cf
ansonfoong <afoong@albany.edu>
2019-01-20 08:09:42 -0500

    Prompt user

--- giftbox.py
+++ giftbox.py
@@ -2,9 +2,11 @@
 
 # This program calculates the price of a gift box. A gift box holds a total of six pieces of chocolate.
 
-darkChocolateHazelNut = 1.40
+darkChocolateHazelNut = 1.40 
 darkChocolateRaspberryCream = 1.10
 milkChocolateOrangeCream = 1
 milkChocolateCaramel = 1.05
 whiteChocolateCoffeeCreams = 1.25
 whiteChocolatePecans = 1.30
+
+print("Welcome to the Candy Shop. You can choose any three varieties of chocolate, creating a box of six chocolates.")
