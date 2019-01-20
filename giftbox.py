bd7c08f87d6856a8f0aaae6f341997628b1afd08
ansonfoong <afoong@albany.edu>
2019-01-20 08:05:34 -0500

    Declare six variables, each resembles a chocolate flavor which stores the price

---
+++ giftbox.py
@@ -0,0 +1,10 @@
+# Anson Foong
+
+# This program calculates the price of a gift box. A gift box holds a total of six pieces of chocolate.
+
+darkChocolateHazelNut = 1.40
+darkChocolateRaspberryCream = 1.10
+milkChocolateOrangeCream = 1
+milkChocolateCaramel = 1.05
+whiteChocolateCoffeeCreams = 1.25
+whiteChocolatePecans = 1.30
