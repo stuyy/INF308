367366c137c9bfa004fd5962fed281f0515f329d
ansonfoong <afoong@albany.edu>
2019-01-20 08:32:57 -0500

    prompt user to enter the amount of nights they are staying.

--- redtide.py
+++ redtide.py
@@ -1,5 +1,11 @@
 # Anson Foong
 
 # This program takes online reservations for the Red Tide Resort
-clientName = input("Hello! Welcome to the Red Tide Resort's website. To get started, please provide your name:")
-print(clientName)
+
+ROOM_RENTAL_PRICE = 125
+SALES_TAX = .075
+HABITATION_TAX = 5
+
+clientName = input("Hello! Welcome to the Red Tide Resort's website. To get started, please provide your name:") # Prompt client for their name.
+nightsReserved = input("How many nights are you planning on staying here at the Red Tide Resort?")
+
