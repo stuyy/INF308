9c8442a93b0cf5060be92d30194b9313966cbdf4
ansonfoong <afoong@albany.edu>
2019-01-20 08:30:55 -0500

    Prompt user for name and print

---
+++ redtide.py
@@ -0,0 +1,5 @@
+# Anson Foong
+
+# This program takes online reservations for the Red Tide Resort
+clientName = input("Hello! Welcome to the Red Tide Resort's website. To get started, please provide your name:")
+print(clientName)
