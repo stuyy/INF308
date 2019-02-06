23eef4f4d572150efd89e8e18802b29fa170c439
ansonfoong <afoong@albany.edu>
2019-02-05 20:54:20 -0500

    Added all of the 10 original answers

--- magiceightball.py
+++ magiceightball.py
@@ -13,4 +13,22 @@ key = input()
 randomNumber = generateNumber()
 
 if randomNumber == 1:
-    print("")
+    print("From what I can see with my own Magic 8-Ball, I would say the chances are in your favor :)")
+elif randomNumber == 2:
+    print("I definitely see it happening for you")
+elif randomNumber == 3:
+    print("There's no doubt about it.")
+elif randomNumber == 4:
+    print("Indeed you will.")
+elif randomNumber == 5:
+    print("Maybe ask again later, I'm busy helping other clients.")
+elif randomNumber == 6:
+    print("Why don't you ask a better question?")
+elif randomNumber == 7:
+    print("I'm the wrong person to ask that question pal.")
+elif randomNumber == 8:
+    print("You're doomed if you're counting on it.")
+elif randomNumber == 9:
+    print("I would not get my hopes up.")
+elif randomNumber == 10:
+    print("From the looks of my Magic 8-ball, it says no.")
